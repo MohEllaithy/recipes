f6ecd856156de90636b3f938640957c948239d21
MohEllaithy <ellaithy80@gmail.com>
2022-05-06 18:57:27 +0200

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'a24426e5-2b88-48ab-9b42-474b83250ace';
+export const MODAL_CLOSE_SEC = 2.5;
